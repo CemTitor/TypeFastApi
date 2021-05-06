0db9b713361087b18eb05db77f3a444251e4e1dd
CemTitor <cemyilmaz463@gmail.com>
2021-05-06 23:52:38 +0300

    update score

--- routes/users.js
+++ routes/users.js
@@ -3,12 +3,6 @@ var router = express.Router();
 
 
 const users=[];
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send(users.sort((user1, user2)=>  user1.score <=user2.score)
-      .slice(0,10));
-});
-
 router.post('/score', function(req, res, next) {
   const {score,userName}= req.body;
   users.push({score: Number(score),userName});
@@ -16,3 +10,18 @@ router.post('/score', function(req, res, next) {
 });
 
 module.exports = router;
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send(users.sort((user1, user2)=> {
+    if (user1.score < user2.score){
+    return 1;
+  }
+  if (user1.score > user2.score){
+    return -1;
+  }
+  // a must be equal to b
+  return 0;
+  }).slice(0,10)
+)
+});
